3facdaf604a31bf2987087ca1ec1f4636867086f
Yuliya Kogol <kogol.julia@gmail.com>
2020-05-21 22:22:17 +0300

    feat: add @babel/plugin-transform-runtime

--- fancy-weather/webpack.config.js
+++ fancy-weather/webpack.config.js
@@ -28,7 +28,8 @@ module.exports = (env, options) => {
                     use: {
                       loader: 'babel-loader',
                       options: {
-                        presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
                       }
                     }
                 },

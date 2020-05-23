f2f526c90d5661ed8602f76cd58699e9f64bd684
Yuliya Kogol <kogol.julia@gmail.com>
2020-05-23 21:42:04 +0300

    feat: add a key for mapbox

--- fancy-weather/api-keys.js
+++ fancy-weather/api-keys.js
@@ -2,5 +2,6 @@ const locationKey = '7e9a9770682175';
 const imageKey = 'ywh_l42oBL8NSTrbDisyZi27RBObZ0nBYylQzhQ2YzE';
 const weatherKey = 'bc7c3c26aa634facb7e073e46eaf4c50';
 const geoCodingKey = '43bd00293c384c5688434a5d7ada152c';
+const mapKey = 'pk.eyJ1IjoiMjItMjIiLCJhIjoiY2thano3MHVmMGZqcDJ5bW5mMTQ4dTV4MCJ9.7pRy3QSLwmf3KFLFs5Q67g';
 
-export {locationKey, imageKey, weatherKey, geoCodingKey};
+export {locationKey, imageKey, weatherKey, geoCodingKey, mapKey};

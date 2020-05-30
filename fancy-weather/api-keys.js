301653bbbafe155f3b11d91aa4691c2d9fbec600
Yuliya Kogol <kogol.julia@gmail.com>
2020-05-30 23:19:31 +0300

    feat: add translation key

--- fancy-weather/api-keys.js
+++ fancy-weather/api-keys.js
@@ -3,5 +3,6 @@ const imageKey = 'ywh_l42oBL8NSTrbDisyZi27RBObZ0nBYylQzhQ2YzE';
 const weatherKey = 'bc7c3c26aa634facb7e073e46eaf4c50';
 const geoCodingKey = '43bd00293c384c5688434a5d7ada152c';
 const mapKey = 'pk.eyJ1IjoiMjItMjIiLCJhIjoiY2thano3MHVmMGZqcDJ5bW5mMTQ4dTV4MCJ9.7pRy3QSLwmf3KFLFs5Q67g';
+const translationKey = 'trnsl.1.1.20200322T155651Z.de98a60e6a99185e.089aea4237b51c6db082c966f27a7895cd1e8b44';
 
-export {locationKey, imageKey, weatherKey, geoCodingKey, mapKey};
+export {locationKey, imageKey, weatherKey, geoCodingKey, mapKey, translationKey};

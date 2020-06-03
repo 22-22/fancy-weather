48b423bc5915a5e1ffa76d9fdd3417d4257158de
Yuliya Kogol <kogol.julia@gmail.com>
2020-06-04 00:41:37 +0300

    refactor: add setDate function

--- fancy-weather/src/js/help-define-time.js
+++ fancy-weather/src/js/help-define-time.js
@@ -1,3 +1,5 @@
+import translationBe from './translation-be.json';
+
 function defineSeason() {
   const seasonIdx = new Date().getMonth();
   const seasons = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn', 'winter'];
@@ -18,11 +20,20 @@ function definepartOfDay(hrs) {
   return partOfDay;
 }
 
-function getTimeInUserLocation() {
-  const date = new Date();
-  const timeNow = date.toLocaleTimeString();
-  document.querySelector('#time').innerHTML = timeNow;
-  return timeNow;
+function setDate(dateNow, weekdayShort, weekdayShortNumber, month, monthNumber) {
+  const lang = localStorage.getItem('lang');
+  document.querySelector('#month').setAttribute('data-i18n', `month.idx${monthNumber}`);
+  document.querySelector('#weekdayShort').setAttribute('data-i18n', `dayShort.idx${weekdayShortNumber}`);
+  if (lang === 'be') {
+    const monthKey = `idx${monthNumber}`;
+    const weekdayShortKey = `idx${weekdayShortNumber}`;
+    document.querySelector('#month').innerHTML = translationBe.month[monthKey];
+    document.querySelector('#weekdayShort').innerHTML = translationBe.dayShort[weekdayShortKey];
+  } else {
+    document.querySelector('#month').innerHTML = month;
+    document.querySelector('#weekdayShort').innerHTML = weekdayShort;
+  }
+  document.querySelector('#date').innerHTML = dateNow;
 }
 
 function getDateInUserLocation() {
@@ -30,13 +41,16 @@ function getDateInUserLocation() {
   const weekdayShort = new Date().toLocaleDateString(lang, { weekday: 'short' });
   const month = new Date().toLocaleDateString(lang, { month: 'long' });
   const dateNow = new Date().toLocaleDateString(lang, { day: 'numeric' });
-  document.querySelector('#weekdayShort').innerHTML = weekdayShort;
-  document.querySelector('#date').innerHTML = dateNow;
-  document.querySelector('#month').innerHTML = month;
   const monthNumber = new Date().getMonth();
-  document.querySelector('#month').setAttribute('data-i18n', `month.idx${monthNumber}`);
   const weekdayShortNumber = new Date().getDay();
-  document.querySelector('#weekdayShort').setAttribute('data-i18n', `dayShort.idx${weekdayShortNumber}`);
+  setDate(dateNow, weekdayShort, weekdayShortNumber, month, monthNumber);
+}
+
+function getTimeInUserLocation() {
+  const date = new Date();
+  const timeNow = date.toLocaleTimeString();
+  document.querySelector('#time').innerHTML = timeNow;
+  return timeNow;
 }
 
 function defineTimeInSearchPlace(timezone) {
@@ -49,17 +63,15 @@ function defineTimeInSearchPlace(timezone) {
 
 function defineDateInSearchPlace(timezone) {
   const lang = localStorage.getItem('lang');
-  const weekdayShort = new Date().toLocaleDateString(lang, { weekday: 'short', timeZone: timezone });
-  const month = new Date().toLocaleDateString(lang, { month: 'long', timeZone: timezone });
   const dateNow = new Date().toLocaleDateString(lang, { day: 'numeric', timeZone: timezone });
-  document.querySelector('#weekdayShort').innerHTML = weekdayShort;
-  document.querySelector('#date').innerHTML = dateNow;
-  document.querySelector('#month').innerHTML = month;
-  const monthNumber = new Date().toLocaleDateString(lang, { month: 'numeric', timeZone: timezone });
-  document.querySelector('#month').setAttribute('data-i18n', `month.idx${monthNumber - 1}`);
+  const weekdayShort = new Date().toLocaleDateString(lang, { weekday: 'short', timeZone: timezone });
   const weekdayShortNumber = new Date().toLocaleDateString(lang, { day: 'numeric', timeZone: timezone });
-  document.querySelector('#weekdayShort').setAttribute('data-i18n', `dayShort.idx${weekdayShortNumber}`);
+  const month = new Date().toLocaleDateString(lang, { month: 'long', timeZone: timezone });
+  // toLocaleDateString() returns a human month number
+  const monthNumber = new Date().toLocaleDateString(lang, { month: 'numeric', timeZone: timezone }) - 1;
+  setDate(dateNow, weekdayShort, weekdayShortNumber, month, monthNumber);
 }
+
 
 export {
   defineSeason, getTimeInUserLocation, getDateInUserLocation,
